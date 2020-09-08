bdfa5e5ab0f943bd6451f8cd9ae0323d5ecac7d0
FayeCarter <fayecarter333@gmail.com>
2020-09-08 11:09:37 +0100

    Add players data

--- components/GameStart.vue
+++ components/GameStart.vue
@@ -3,10 +3,27 @@
     <button>StartGame</button>
     <form action="/action_page.php">
       <label for="players">Select Players (between 1 and 2):</label>
-      <input id="players" type="number" name="quantity" min="1" max="2" />
+      <input
+        id="players"
+        v-model="players"
+        type="number"
+        name="quantity"
+        min="1"
+        max="2"
+      />
       <input type="submit" value="Play" />
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      players: '1',
+    }
+  },
+}
+</script>
 
 <style></style>
